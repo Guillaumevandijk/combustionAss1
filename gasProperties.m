d17c6b600368cf09dba30119327e6b7e82817f29
Guillaumevandijk <guillaumevandijk@gmail.com>
2023-05-11 15:44:38 +0200

    add

---
+++ gasProperties.m
@@ -0,0 +1,7 @@
+
+%formation enthalpies
+
+Hf_NO = 90.25; %kJ/mol
+S_NO = 210.76; %kJ/K/mol
+R_0 = 8.314; %J/K/mol
+
--- q3.m
+++ q3.m
@@ -1,12 +1,36 @@
 syms z
 
+gasProperties
+
+phi = 0.4;
+
+T = getT(phi);
 
 %input
 phi = 0.8;
 
-%find Kp with H and S
-Kp = 0.012;
+deltaH = -Hf_NO;
+deltaS = -S_NO;
 
+deltaG = deltaH - T*deltaS/1000;
+
+Kp = exp(-deltaG*1000/T/R_0);
+
+%find Kp with H and S
 %Solve for z
-getz = Kp == 3*z/(3.76*3/phi - z)/(3/phi-3-z);
-z = double(min(solve(getz,z)));
+getz = Kp == z^2/(1-z)^2;
+%z = double(min(solve(getz,z)));
+z = double(max(solve(getz,z)));
+
+
+function T = getT(phi)
+    Tphi = [0.4,   1623.8;
+            0.6,   2028.8;
+            0.8,   2331.7;
+            0.99,  2482.9;];
+
+    T = interp1(Tphi(:,1),Tphi(:,2),phi);
+
+
+end
+
