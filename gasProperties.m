174aa08890236de264c18fe20b7651e63448d19b
Guillaumevandijk <guillaumevandijk@gmail.com>
2023-05-15 20:42:19 +0200

    add

--- gasProperties.m
+++ gasProperties.m
@@ -2,6 +2,18 @@
 %formation enthalpies
 
 Hf_NO = 90.25; %kJ/mol
+
 S_NO = 210.76; %kJ/K/mol
+S_O2 = 205.1; %kJ/K/mol
+S_N2 = 191.6; %kJ/K/mol
+
+%Gas constant
 R_0 = 8.314; %J/K/mol
 
+%Molar masses
+
+W_NO = 14+16;
+W_N2 = 14*2;
+W_O2 = 16*2;
+W_CO2 = 12.01+2*16;
+W_H2O = 1.008*2+16;
